2cfb4df6db2ac72a5b081848aaee6b6ddf816de2
Steve Cohen <cohens@codeaurora.org>
2017-12-11 19:23:47 -0500

    drm/msm: update context locking for 4.14
    This change updates the SDE and patched MSM drivers to use
    local context locking provided by @drm_modeset_acquire_ctx
    since some legacy locking API functions are now deprecated.
    
    Change-Id: I80bf8198a31e1de43f2a1312b4828e5ecb7a3467
    Signed-off-by: Steve Cohen <cohens@codeaurora.org>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- drivers/gpu/drm/msm/msm_kms.h
+++ drivers/gpu/drm/msm/msm_kms.h
@@ -96,7 +96,8 @@ struct msm_kms_funcs {
 	void (*postopen)(struct msm_kms *kms, struct drm_file *file);
 	void (*preclose)(struct msm_kms *kms, struct drm_file *file);
 	void (*postclose)(struct msm_kms *kms, struct drm_file *file);
-	void (*lastclose)(struct msm_kms *kms);
+	void (*lastclose)(struct msm_kms *kms,
+			struct drm_modeset_acquire_ctx *ctx);
 	int (*register_events)(struct msm_kms *kms,
 			struct drm_mode_object *obj, u32 event, bool en);
 	/* pm suspend/resume hooks */
