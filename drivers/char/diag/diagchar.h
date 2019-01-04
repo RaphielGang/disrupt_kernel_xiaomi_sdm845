ad906b422c7b6f529a5ef5dafadb310e373229d7
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2019-01-04 11:14:16 +0530

    diag: Increase the MHI buffer size to 4k
    Increase the buffer size to 4k for the buffers submitted to MHI
    layer.
    
    Change-Id: Iec8f582a986f69ef078174a57ed4d062ac6ccfbb
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <raphierion@raphielgang.org>

--- drivers/char/diag/diagchar.h
+++ drivers/char/diag/diagchar.h
@@ -267,9 +267,19 @@ for (i = 0; i <= fwd_info->num_pd - 2; i++)	\
 
 #define DIAG_TS_SIZE	50
 
+#ifdef CONFIG_MHI_BUS
+#define DIAG_MDM_BUF_SIZE	4096
+#else
 #define DIAG_MDM_BUF_SIZE	2048
+#endif
+
+/* The Maximum request size is 4k + DCI header + footer (6 bytes) */
+#ifdef CONFIG_MHI_BUS
+#define DIAG_MDM_DCI_BUF_SIZE	(4096 + 6)
+#else
 /* The Maximum request size is 2k + DCI header + footer (6 bytes) */
 #define DIAG_MDM_DCI_BUF_SIZE	(2048 + 6)
+#endif
 
 #define DIAG_LOCAL_PROC	0
 
