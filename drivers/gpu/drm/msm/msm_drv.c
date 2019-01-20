d7ea6eac0a63b852a77d840b4e3b20ccb35e628e
Maria Yu <aiquny@codeaurora.org>
2019-01-20 20:42:06 +0800

    drm/msm/sde: Fix for match NULL handle in msm_pdev_probe
    Mem abort happened when match is NULL in msm_pdev_probe
    processing. Fix it by gracefully checking.
    
    Change-Id: Ie66b5bbad39dc180366585d51c9ac4890633b2c6
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
  * Copyright (C) 2018 XiaoMi, Inc.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
@@ -2076,6 +2076,9 @@ static int msm_pdev_probe(struct platform_device *pdev)
 	ret = add_bridge_components(&pdev->dev, &match);
 	if (ret)
 		return ret;
+
+	if (!match)
+		return -ENODEV;
 
 	pdev->dev.coherent_dma_mask = DMA_BIT_MASK(32);
 	return component_master_add_with_match(&pdev->dev, &msm_drm_ops, match);
