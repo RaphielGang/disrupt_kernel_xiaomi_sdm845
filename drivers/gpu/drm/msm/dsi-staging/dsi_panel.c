106eeec1152af054dabdf4377d83a7fcb06e2e86
Dhaval Patel <pdhaval@codeaurora.org>
2019-03-07 20:20:17 +0530

    drm/msm/dsi-staging: fix incorrect handling of panel framerate
    For some panels, panel get more prone to failures due to incorrect
    handling of panel framerate failures. This change fixes the same
    by returning this failures as fatal error in panel binding.
    
    Change-Id: Ie5x666b896f2d686636fac7k8c45d1fs3r5b2t0r
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1155,6 +1155,8 @@ static int dsi_panel_parse_timing(struct device *parent,
 		goto error;
 	}
 
+	BUG_ON(mode->refresh_rate != 60);
+
 	rc = dsi_panel_parse(of_node, fw_entry,
 		"qcom,mdss-dsi-panel-width", &mode->h_active);
 	if (rc) {
