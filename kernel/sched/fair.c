998773ba80bbd9e270d7084049c6ac10c519166d
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-04-22 15:18:53 +0700

    Revert "ANDROID: sched/fair: add idle state filter to prefer_idle case"
    This reverts commit cdc58175974967a3534ed1e93375073df24de9b7.
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7012,7 +7012,6 @@ retry:
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util, min_capped_util;
 			long spare_cap;
-			int idle_idx = INT_MAX;
 
 			cpumask_clear_cpu(i, &search_cpus);
 
@@ -7067,10 +7066,6 @@ retry:
 			 */
 			spare_cap = capacity_orig - new_util;
 
-			if (idle_cpu(i))
-				idle_idx = idle_get_state_idx(cpu_rq(i));
-
-
 			/*
 			 * Case A) Latency sensitive tasks
 			 *
@@ -7114,18 +7109,13 @@ retry:
 					schedstat_inc(this_rq()->eas_stats.fbt_pref_idle);
 
 					if (boosted &&
-					    capacity_orig < target_capacity)
+					    capacity_orig <= target_capacity)
 						continue;
 					if (!boosted &&
-					    capacity_orig > target_capacity)
-						continue;
-					if (capacity_orig == target_capacity &&
-					    sysctl_sched_cstate_aware &&
-					    best_idle_cstate <= idle_idx)
+					    capacity_orig >= target_capacity)
 						continue;
 
 					target_capacity = capacity_orig;
-					best_idle_cstate = idle_idx;
 					best_idle_cpu = i;
 					continue;
 				}
@@ -7201,6 +7191,7 @@ retry:
 			 * consumptions without affecting performance.
 			 */
 			if (idle_cpu(i)) {
+				int idle_idx = idle_get_state_idx(cpu_rq(i));
 
 				/* Favor CPUs that won't end up running at a
 				 * high OPP.
