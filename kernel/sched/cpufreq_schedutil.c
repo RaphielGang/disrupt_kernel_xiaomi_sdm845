66343e19e8b242d91ffd546d3c96706fb2bb544e
Danny Lin <danny@kdrag0n.dev>
2019-08-05 06:45:05 +0000

    cpufreq: schedutil: Expose default rate-limits as config options
    This allows us to tune the default rate-limits without constantly
    changing schedutil's code.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    
    [raphielscape] : Trim cpufreq Kconfig spaces, well, VSCode do this.
    
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -550,7 +550,7 @@ static void update_min_rate_limit_ns(struct sugov_policy *sg_policy)
 static ssize_t up_rate_limit_us_show(struct gov_attr_set *attr_set, char *buf)
 {
  	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
- 
+
 	return sprintf(buf, "%u\n", tunables->up_rate_limit_us);
 }
 
@@ -872,8 +872,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	 * intializing up_rate/down_rate to 0 explicitly in kernel
 	 * since WALT expects so by default.
 	 */
-	tunables->up_rate_limit_us = 0;
-	tunables->down_rate_limit_us = 0;
+	tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
+	tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
